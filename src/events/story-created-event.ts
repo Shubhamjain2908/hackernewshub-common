3257f0db431dbcb440c99e002c1d0473362a7729
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-13 03:11:02 +0530

    added StoryEventModel

--- src/events/story-created-event.ts
+++ src/events/story-created-event.ts
@@ -3,11 +3,15 @@ import { Subjects } from './subjects';
 export interface StoryCreatedEvent {
     subject: Subjects.StoryCreated;
     data: {
-        storyId: number,
-        title: string,
-        url: string,
-        createdAt: number,
-        user: string,
-        comments: Array<number>
+        story: Array<StoryEventModel>
     };
+}
+
+export interface StoryEventModel {
+    storyId: number,
+    title: string,
+    url: string,
+    createdAt: number,
+    user: string,
+    comments: Array<number>
 }
